4afbc7228b58ba0358f0d463ba776ea0071b4c3d
Aaron C <afcofiel@umich.edu>
2019-03-29 13:38:17 -0400

    Added image sending through socket and changed html for directory selection

--- server/index.js
+++ server/index.js
@@ -8,32 +8,69 @@ const htmldir = __dirname + '/../www/';
 
 define_gets();
 
-http.listen(8181,function(){
+var frame_num = -1;
+
+io.on('connection', function (socket) {
+    console.log("user connected");
+    socket.on('next_image', function (req) {
+        try {
+            frame_num = frame_num + 1;
+            var image_buffer = fs.readFileSync(htmldir + "../data/example_set/Left/Pic" + frame_num.toString() + ".bmp");
+            socket.emit('image', {found: true, frame: frame_num, buffer: image_buffer.toString('base64')});
+            console.log('sent_image');
+        }
+        catch (e) {
+            frame_num = frame_num - 1;
+            socket.emit('FNF',{exception:e});
+            console.log("File not found");
+        }
+    });
+
+    socket.on('prev_image', function (req) {
+        try {
+            frame_num = frame_num - 1;
+            var image_buffer = fs.readFileSync(htmldir + "../data/example_set/Left/Pic" + frame_num.toString() + ".bmp");
+            socket.emit('image', {found: true, frame: frame_num, buffer: image_buffer.toString('base64')});
+            console.log('sent_image');
+        }
+        catch (e) {
+            frame_num = frame_num + 1;
+            socket.emit('FNF',{exception:e});
+            console.log("File not found");
+        }
+    });
+});
+
+http.listen(8181, function () {
     console.log('listening on: 8181');
 });
 
-function define_gets(){
-    app.get('/',function(req,res){
+function define_gets() {
+    app.get('/', function (req, res) {
         res.sendFile(path.resolve(htmldir + "index.html"));
     });
 
-    app.get('/js/main.js',function(req,res){
+    app.get('/start.bmp', function (req, res) {
+        res.sendFile(path.resolve(htmldir + "start.bmp"));
+    });
+
+    app.get('/js/main.js', function (req, res) {
         res.sendFile(path.resolve(htmldir + "js/main.js"));
     });
 
-    app.get('/key.txt',function (req,res) {
+    app.get('/key.txt', function (req, res) {
         res.sendFile(path.resolve(htmldir + "key.txt"));
     });
 
-    app.get('/vendor/jquery/jquery.min.js',function(req,res){
+    app.get('/vendor/jquery/jquery.min.js', function (req, res) {
         res.sendFile(path.resolve(htmldir + "vendor/jquery/jquery.min.js"));
     });
 
-    app.get('/vendor/bootstrap/js/bootstrap.bundle.min.js',function(req,res){
+    app.get('/vendor/bootstrap/js/bootstrap.bundle.min.js', function (req, res) {
         res.sendFile(path.resolve(htmldir + "vendor/bootstrap/js/bootstrap.bundle.min.js"));
     });
 
-    app.get('/vendor/bootstrap/css/bootstrap.min.css',function(req,res){
+    app.get('/vendor/bootstrap/css/bootstrap.min.css', function (req, res) {
         res.sendFile(path.resolve(htmldir + "vendor/bootstrap/css/bootstrap.min.css"));
     });
 }
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,35 @@
+var markers = [];
+var frame_num = 0;
+var socket = io();
+
+function main() {
+    var key;
+    jQuery.ajax({
+        url:"http://localhost:8181/key.txt",
+        success: function (result) {
+            key = result;
+        },
+        async: false
+    });
+    $.getScript("https://maps.googleapis.com/maps/api/js?key="+ key +"&callback=myMap");
+    socket.on('image', function(msg){
+        document.getElementById('img_seq_frame').setAttribute("src","data:image/bmp;base64," + msg.buffer);
+        console.log("Got image");
+        frame_num = msg.frame;
+        document.getElementById('frame_num').innerHTML = "Frame Number: " + frame_num;
+
+    });
+}
+
+function get_next_img(){
+    socket.emit('next_image', {'web_frame_num':frame_num});
+}
+function get_prev_image(){
+    socket.emit('prev_image', {'web_frame_num':frame_num});
+}
+
+$(document).ready(main);
+
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(42.319655, -83.232128),
@@ -11,24 +43,27 @@ function myMap() {
     });
     map.setTilt(0);
 }
-var maker_locations = [];
-
 function placeMarker(map, location) {
     var marker = new google.maps.Marker({
         position: location,
         map: map
     });
-    maker_locations.push(location);
-
-
-        //location.lat() location.lng()
+    makers.push(marker);
+    //location.lat() location.lng()
 }
-var key;
-jQuery.ajax({
-    url:"http://localhost:8181/key.txt",
-    success: function (result) {
-        key = result;
-    },
-    async: false
-});
-$.getScript("https://maps.googleapis.com/maps/api/js?key="+ key +"&callback=myMap");
+document.onkeydown = function(e) {
+    switch (e.key) {
+        case "ArrowLeft":
+            get_prev_image();
+            break;
+        case "ArrowUp":
+            // alert('up');
+            break;
+        case "ArrowRight":
+            get_next_img()
+            break;
+        case "ArrowDown":
+            // alert('down');
+            break;
+    }
+};
